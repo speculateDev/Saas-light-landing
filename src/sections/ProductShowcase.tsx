aaeb97c74d33bd231f19a56dfc5f133ae9fe4af3
Speculate Dev <sida3li777@gmail.com>
2025-05-29 17:35:31 +0100

    Added ProductSection animation

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,28 @@
+"use client";
+
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import productImage from "@/assets/product-image.png";
 import pyramidImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"], // [el, viewport]
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-[#fff] to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-[#fff] to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading">
           <div className="flex justify-center">
@@ -23,19 +40,26 @@ export const ProductShowcase = () => {
         <div className="relative">
           <Image src={productImage} alt="product image" className="mt-10" />
 
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt="Pyramid image"
             height={262}
             width={262}
             className="hidden md:block absolute -top-32 -right-36"
+            style={{
+              translateY: translateY,
+            }}
           />
 
-          <Image
-            src={tubeImage}
+          <motion.img
+            src={tubeImage.src}
             alt="Tube image"
             height={248}
+            width={248}
             className="hidden md:block absolute -left-36 bottom-24"
+            style={{
+              translateY,
+            }}
           />
         </div>
       </div>
